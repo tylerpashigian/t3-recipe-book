7197d8a8bdc0c12bbe0c1938cde91cd0654d6b6c
Tyler Pashigian <tylerpashigian@gmail.com>
2025-05-09 08:17:36 -0400

    Adding temporary loading state to SSG pages

---
+++ src/app/recipe/[id]/loading.tsx
@@ -0,0 +1,9 @@
+const Loading = () => {
+  return (
+    <div className="flex h-full w-full flex-col items-center gap-2">
+      <p className="text-2xl">Loading...</p>
+    </div>
+  );
+};
+
+export default Loading;
